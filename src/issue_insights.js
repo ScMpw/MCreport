af21aa019ad5ced3d192291e80569c9dbe88e327
ScMpw <philipp.wagner@gebit.de>
2025-11-21 14:33:13 +0100

    Use Jira JQL search endpoint

--- src/issue_insights.js
+++ src/issue_insights.js
@@ -293,7 +293,7 @@
   }
 
   async function jiraSearch(jql, fields = [], options = {}) {
-    const searchUrl = `https://${jiraDomain}/rest/api/3/search`;
+    const searchUrl = `https://${jiraDomain}/rest/api/3/search/jql`;
     const maxResults = options.maxResults || 500;
     let startAt = options.startAt || 0;
     const collected = [];
@@ -301,7 +301,6 @@
     const expandList = Array.isArray(options.expand)
       ? options.expand.filter(Boolean)
       : (options.expand ? [options.expand] : []);
-    let useGet = true;
 
     const buildPayload = () => {
       const payload = { jql, startAt, maxResults };
@@ -313,43 +312,18 @@
     while (true) {
       let resp;
       try {
-        if (useGet) {
-          const params = new URLSearchParams();
-          params.set('jql', jql);
-          params.set('startAt', String(startAt));
-          params.set('maxResults', String(maxResults));
-          if (fieldList.length) params.set('fields', fieldList.join(','));
-          if (expandList.length) params.set('expand', expandList.join(','));
-          const url = `${searchUrl}?${params.toString()}`;
-          resp = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-            headers: { 'Accept': 'application/json' }
-          });
-        } else {
-          resp = await fetch(searchUrl, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'X-Atlassian-Token': 'no-check'
-            },
-            body: JSON.stringify(buildPayload())
-          });
-        }
+        resp = await fetch(searchUrl, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-Atlassian-Token': 'no-check'
+          },
+          body: JSON.stringify(buildPayload())
+        });
       } catch (err) {
-        if (useGet) {
-          console.warn('Jira search GET failed, retrying with POST', err);
-          useGet = false;
-          continue;
-        }
         throw err;
-      }
-
-      if (useGet && [405, 413, 414].includes(resp.status)) {
-        useGet = false;
-        continue;
       }
 
       if (!resp.ok) {
