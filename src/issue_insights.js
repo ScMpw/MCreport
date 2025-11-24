b9d8bbc42d6d7cb6f73dd2dd03c051d7a5475159
ScMpw <philipp.wagner@gebit.de>
2025-11-24 09:47:25 +0100

    Use Jira search/jql endpoint for sprint insights

--- src/issue_insights.js
+++ src/issue_insights.js
@@ -365,7 +365,7 @@
   }
 
   async function jiraSearch(jql, fields = [], options = {}) {
-    const searchUrl = `https://${jiraDomain}/rest/api/3/search`;
+    const searchUrl = `https://${jiraDomain}/rest/api/3/search/jql`;
     const maxResults = options.maxResults || 500;
     let startAt = options.startAt || 0;
     const collected = [];
@@ -373,7 +373,6 @@
     const expandList = Array.isArray(options.expand)
       ? options.expand.filter(Boolean)
       : (options.expand ? [options.expand] : []);
-    let useGet = true;
 
     const buildPayload = () => {
       const payload = { jql, startAt, maxResults };
@@ -382,50 +381,21 @@
       return payload;
     };
 
-    const buildParams = () => {
-      const params = new URLSearchParams();
-      params.set('jql', jql);
-      params.set('startAt', String(startAt));
-      params.set('maxResults', String(maxResults));
-      if (fieldList.length) params.set('fields', fieldList.join(','));
-      if (expandList.length) params.set('expand', expandList.join(','));
-      return params.toString();
-    };
-
     while (true) {
       let resp;
       try {
-        if (useGet) {
-          const url = `${searchUrl}?${buildParams()}`;
-          resp = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-            headers: { 'Accept': 'application/json' }
-          });
-        } else {
-          resp = await fetch(searchUrl, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'X-Atlassian-Token': 'no-check'
-            },
-            body: JSON.stringify(buildPayload())
-          });
-        }
+        resp = await fetch(searchUrl, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-Atlassian-Token': 'no-check'
+          },
+          body: JSON.stringify(buildPayload())
+        });
       } catch (err) {
-        if (useGet) {
-          console.warn('Jira search GET failed, retrying with POST', err);
-          useGet = false;
-          continue;
-        }
         throw new Error(buildNetworkErrorMessage(err, 'Unable to reach Jira search'));
-      }
-
-      if (useGet && [405, 413, 414].includes(resp.status)) {
-        useGet = false;
-        continue;
       }
 
       if (!resp.ok) {
